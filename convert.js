9acb167ec52a1d9fa8ec0079538a2fdb5ee59d8e
Manzoorahmed79 <165725238+Manzoorahmed79@users.noreply.github.com>
2024-08-06 12:19:08 -0700

    Update convert.js

--- convert.js
+++ convert.js
@@ -13,13 +13,13 @@ submit.addEventListener('click', () => {
   const date = new Date();
   const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
   const formattedTime = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
-  const obfuscationComment = `<!-- Obfuscated at ${formattedDate} ${formattedTime} on https://bj-javascript-obfuscator.vercel.app/ ( Powered By Mr,BaBlU ) -->`;
+  const obfuscationComment = `<!-- Obfuscated at ${formattedDate} ${formattedTime} on https://bj-javascript-obfuscator.vercel.app/ ( Powered By Mr,BaBlU ) -->\n`;
 
   if (type.value == 'js64') {
-    output.value = `<script>document.write(atob("${btoa(inputString)}"));</script>\n${obfuscationComment}`;
+    output.value = `${obfuscationComment}<script>document.write(atob("${btoa(inputString)}"));</script>`;
   } else if (type.value == 'jsc') {
     const charCodesArray = stringToCharCodes(inputString);
-    output.value = `<script>var y="";var x=[${charCodesArray}];x.forEach(char=>{y+=String.fromCharCode(char)});document.write(y);</script>\n${obfuscationComment}`;
+    output.value = `${obfuscationComment}<script>var y="";var x=[${charCodesArray}];x.forEach(char=>{y+=String.fromCharCode(char)});document.write(y);</script>`;
   }
 });
 

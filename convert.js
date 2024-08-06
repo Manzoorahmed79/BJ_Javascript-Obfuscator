1e0f277d129bf60b3c6938b8b3675c78b85e2aca
Manzoorahmed79 <165725238+Manzoorahmed79@users.noreply.github.com>
2024-08-06 12:17:09 -0700

    Update convert.js

--- convert.js
+++ convert.js
@@ -10,12 +10,16 @@ submit.addEventListener('click', () => {
   }
 
   const inputString = input.value;
+  const date = new Date();
+  const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+  const formattedTime = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
+  const obfuscationComment = `<!-- Obfuscated at ${formattedDate} ${formattedTime} on https://bj-javascript-obfuscator.vercel.app/ ( Powered By Mr,BaBlU ) -->`;
 
   if (type.value == 'js64') {
-    output.value = `<script>document.write(atob("${btoa(inputString)}"));</script>`;
+    output.value = `<script>document.write(atob("${btoa(inputString)}"));</script>\n${obfuscationComment}`;
   } else if (type.value == 'jsc') {
     const charCodesArray = stringToCharCodes(inputString);
-    output.value = `<!-- Obfuscated at 2024-08-06 12:09:54 on https://bj-javascript-obfuscator.vercel.app/ ( Powered By Mr,BaBlU ) --><script>var y="";var x=[${charCodesArray}];x.forEach(char=>{y+=String.fromCharCode(char)});document.write(y);</script>`;
+    output.value = `<script>var y="";var x=[${charCodesArray}];x.forEach(char=>{y+=String.fromCharCode(char)});document.write(y);</script>\n${obfuscationComment}`;
   }
 });
 
